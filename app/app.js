b1c71e1bad23d4c614f779c1a8244f3a2ce5ab09
JRitzenhoff <ritzenhoffjlhs@gmail.com>
2020-03-21 19:21:52 +0100

    Made app Progressive Web App compatile

--- app/app.js
+++ app/app.js
@@ -15,18 +15,6 @@ app.use(
 	})
 );
 
-// ovverride static index.html preference
-app.get('/', (req, res) => {
-	res.send("this is home");
-})
-
-app.get('/favicon.ico', (req, res) => {
-	res.sendFile(path.resolve(__dirname) + '/favicon.ico');
-});
-
-// tells express to serve static files from the directory named "public"
-app.use(express.static('public'));
-
 app.get('/getBettenanzahl/:hospitalName', db.getHospitalBedsByName);
 app.get('/getFreieBetten/:hospitalName', db.getFreeHospitalBedsByName);
 
@@ -38,6 +26,24 @@ app.put('/setFreieBetten/:hospitalName', db.setFreeHospitalBedsByName);
 
 app.put('/incrementBettenanzahl/:hospitalName', db.incrementHospitalBedsByName);
 app.put('/incrementFreieBetten/:hospitalName', db.incrementFreeHospitalBedsByName);
+
+
+// NOTE:
+// Overriding prevents the href in the index.html from being called... 
+//	Which never actually points Chrome to the manifest.json file
+
+// // ovverride static index.html preference
+// app.get('/', (req, res) => {
+// 	// res.sendFile('/index.html');
+// 	res.send("this is home");
+// })
+
+// tells express to serve static files from the directory named "public"
+app.use(express.static('public'));
+
+app.get('/favicon.ico', (req, res) => {
+	res.sendFile(path.resolve(__dirname) + '/favicon.ico');
+});
 
 
 // 404 response for all unspecified routes
