7869350224c5a1a6530ec1776ac3b9e36327cb10
Matthias Ritzenhoff <ma.ritzenhoff@tum.de>
2020-03-29 23:38:31 +0200

    Added dummy data

--- database/create_dummies.sql
+++ database/create_dummies.sql
@@ -5,4 +5,13 @@ INSERT INTO employee VALUES(0,'Meister','Ludwigstraße 23, 85375 Neufahrn b. Fre
 
 INSERT INTO hospitals VALUES (1,'Easy','dummy Adresse', 0, 'gibt keine, Hamsterkäufe',0,100);
 
+
 INSERT INTO hospitals VALUES (3,'Diakonie-Krankenhaus Wehrda','Hebronberg 5', 1, 'https://www.dkh-wehrda.de/startseite/',0,100);
+
+INSERT INTO cities VALUES (1, 'Marburg', 'Hessen', 0);
+
+
+
+
+
+INSERT INTO hospitals VALUES (4,'Universitätsklinikum Gießen und Marburg','Baldingerstraße', 1, 'https://www.ukgm.de/home.html',200,100);
