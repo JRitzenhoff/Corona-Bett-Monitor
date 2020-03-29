453e31aebc7edc8ef8d20164b4e91d95138c124b
Matthias Ritzenhoff <ma.ritzenhoff@tum.de>
2020-03-29 19:47:45 +0200

    Delted non relevant parts in new_setBeds do resolve errors

--- database/create_dummies.sql
+++ database/create_dummies.sql
@@ -4,3 +4,5 @@ INSERT INTO hospitals VALUES (0,'Klinkum Rechts der Isar','dummy Adresse', 0, 'g
 INSERT INTO employee VALUES(0,'Meister','Ludwigstraße 23, 85375 Neufahrn b. Freising','hanz@meier.de','lalala',0);
 
 INSERT INTO hospitals VALUES (1,'Easy','dummy Adresse', 0, 'gibt keine, Hamsterkäufe',0,100);
+
+INSERT INTO hospitals VALUES (3,'Diakonie-Krankenhaus Wehrda','Hebronberg 5', 1, 'https://www.dkh-wehrda.de/startseite/',0,100);
