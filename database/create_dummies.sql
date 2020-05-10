f2d848e1c363d2f8b2fe758ceeb3de2feefbcf90
Matthias Ritzenhoff <matthias@matthiass-mbp.speedport.ip>
2020-05-10 23:11:33 +0200

    corrected scalling

--- database/create_dummies.sql
+++ database/create_dummies.sql
@@ -6,11 +6,11 @@ INSERT INTO cities VALUES (2, 'Köln', 'Nordrhein-Westfalen', 0);
 INSERT INTO cities VALUES (3, 'Stuttgart', 'Baden-Württemberg', 0);
 INSERT INTO cities VALUES (4, 'Hamburg', 'Hamburg', 0);
 
-INSERT INTO employee VALUES(0,'Klinkum_Rechts_der_Isar','Kein Addresse','Kein email','kennwort',1);
+INSERT INTO employee VALUES(0,'Klinikum_Rechts_der_Isar','Kein Addresse','Kein email','kennwort',1);
 INSERT INTO employee VALUES(1,'Meister','Ludwigstraße 23, 85375 Neufahrn b. Freising','hanz@meier.de','lalala',2);
 
 INSERT INTO hospitals VALUES (0,'Universitätsklinikum Hamburg-Eppendorf','Martinstrasse 52', 4, 'https://www.uke.de/index.html',120,17);
-INSERT INTO hospitals VALUES (1,'Klinkum Rechts der Isar','Ismaninger Str. 22', 0, 'https://www.mri.tum.de/',150,130);
+INSERT INTO hospitals VALUES (1,'Klinikum Rechts der Isar','Ismaninger Str. 22', 0, 'https://www.mri.tum.de/',150,130);
 INSERT INTO hospitals VALUES (2,'Diakonie-Krankenhaus Wehrda','Hebronberg 5', 1, 'https://www.dkh-wehrda.de/startseite/',50,43);
 INSERT INTO hospitals VALUES (3,'Universitätsklinikum Gießen und Marburg','Baldingerstraße', 1, 'https://www.ukgm.de/home.html',60,56);
 INSERT INTO hospitals VALUES (4,'Uniklinik Köln','Kerpener Str. 62', 2, 'https://www.uk-koeln.de/',90,81);
